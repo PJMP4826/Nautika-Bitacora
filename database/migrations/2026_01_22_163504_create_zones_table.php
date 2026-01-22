1b2fb3cc7ea679d75a2e263ccc867c30472a8b7d
CoderFausto <residente01@intelisof.com.mx>
2026-01-22 14:07:51 -0600

    feat(migration): add zones table

---
+++ database/migrations/2026_01_22_163504_create_zones_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('zones', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('region')->nullable();
+            $table->enum('water_type', ['mar', 'rio', 'lago']);
+            $table->boolean('active');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('zones');
+    }
+};
