f5164d2c18e0ecfc60fdfc8c2a04105e1b4beda8
CoderFausto <residente01@intelisof.com.mx>
2026-01-22 19:31:34 -0600

    feat(controller): add controller generico para Zone

---
+++ app/Http/Controllers/ZoneController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ZoneRequest;
+use App\Http\Resources\ZoneResource;
+use App\Models\Zone;
+
+class ZoneController extends Controller
+{
+    public function index()
+    {
+        return ZoneResource::collection(Zone::all());
+    }
+
+    public function store(ZoneRequest $request)
+    {
+        return new ZoneResource(Zone::create($request->validated()));
+    }
+
+    public function show(Zone $zone)
+    {
+        return new ZoneResource($zone);
+    }
+
+    public function update(ZoneRequest $request, Zone $zone)
+    {
+        $zone->update($request->validated());
+
+        return new ZoneResource($zone);
+    }
+
+    public function destroy(Zone $zone)
+    {
+        $zone->delete();
+
+        return response()->json();
+    }
+}
