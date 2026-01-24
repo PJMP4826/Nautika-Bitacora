a9c26b08a459aab72014fddc6ea57eed4b2f3885
CoderFausto <residente01@intelisof.com.mx>
2026-01-23 19:15:54 -0600

    feat(home): integrate FishingDataService into HomeController for dynamic data rendering

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\FishingDataService;
 use Inertia\Inertia;
 
 class HomeController extends Controller
 {
+    private FishingDataService $fishingDataService;
+
+    public function __construct(
+        FishingDataService $fishingDataService
+    ) {
+        $this->fishingDataService = $fishingDataService;
+    }
+
     public function index()
     {
-        return Inertia::render('Home');
+        return Inertia::render('Home', [
+            'fishingTypes' => $this->fishingDataService->getFishingTypes(),
+            'experienceLevels' => $this->fishingDataService->getExperienceLevels(),
+            'seasons' => $this->fishingDataService->getSeasons(),
+        ]);
     }
 }
