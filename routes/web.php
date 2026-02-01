87916a85634698f5be398c44c6cf7b1ca33cee92
CoderFausto <residente01@intelisof.com.mx>
2026-02-01 16:40:17 -0600

    feat(routes): add routes for fish index and show methods in FishController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FishController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ZoneController;
 use App\Http\Controllers\NotFound;
@@ -8,5 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/zones', [ZoneController::class, 'index'])->name('zone');
 Route::get('/zones/{zone_name}', [ZoneController::class, 'show'])->name('zone.show');
+Route::get('/fish', [FishController::class, 'index'])->name('fish');
+Route::get('/fish/{fish_slug}', [FishController::class, 'show'])->name('fish.show');
 
 Route::fallback([NotFound::class, 'index']);
