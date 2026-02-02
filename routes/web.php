95b80151ff1d458e62845410e5f51969c9c013ea
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-02-02 14:08:41 -0600

    feat(routes): add search route for SearchController to handle search functionality

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,10 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ZoneController;
 use App\Http\Controllers\NotFound;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SearchController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/search', [SearchController::class, 'search'])->name('search');
 Route::get('/zones', [ZoneController::class, 'index'])->name('zone');
 Route::get('/zones/{zone_name}', [ZoneController::class, 'show'])->name('zone.show');
 Route::get('/fish', [FishController::class, 'index'])->name('fish');
