00c7c1fd23fd029b3de0d0ce3a56745d61fc8883
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-02-23 17:43:44 -0600

    fix(NotFoundController): cambia la ruta de la vista que renderiza inertia.js

--- app/Http/Controllers/NotFound.php
+++ app/Http/Controllers/NotFound.php
@@ -9,6 +9,6 @@ class NotFound extends Controller
 {
     public function index()
     {
-        return Inertia::render('errors/NotFound');
+        return Inertia::render('landing/errors/NotFound');
     }
 }
