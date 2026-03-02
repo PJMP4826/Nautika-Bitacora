7993f9b92c183b2ff9418bd13ccd3e11e341e935
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-03-01 22:02:43 -0600

    refactor(admin): remove unused NavFooter component from AppSidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -86,7 +86,6 @@ export function AppSidebar() {
             </SidebarContent>
 
             <SidebarFooter>
-                <NavFooter items={footerNavItems} className="mt-auto" />
                 <NavUser />
             </SidebarFooter>
         </Sidebar>
