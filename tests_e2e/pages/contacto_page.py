57fa4e4999f2e4ba31bb2df349438794c6ff07df
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-07-26 18:51:31 -0600

    feat(tests): añadir la página de contacto para pruebas e2e
    - implementar la clase ContactoPage con los selectores del formulario
    - definir los métodos para interactuar con los campos de entrada
    - agregar la lógica para navegar a la url de contacto y validar el envío

---
+++ tests_e2e/pages/contacto_page.py
@@ -0,0 +1,37 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+
+
+class ContactoPage(BasePage):
+    _TITLE_LABEL = (By.CSS_SELECTOR, "h2.text-4xl")
+    _NAME_INPUT = (By.ID, "name")
+    _LAST_NAME_INPUT = (By.ID, "last-name")
+    _EMAIL_INPUT = (By.ID, "email")
+    _PHONE_NUMBER_INPUT = (By.ID, "phone-number")
+    _MESSAGE_INPUT = (By.ID, "your-message")
+    _SUBMIT_BUTTON = (By.ID, "submit-button")
+
+    _SUCCESS_MODAL_TITLE = (By.XPATH, "//h3[text()='¡Mensaje enviado!']")
+    def __init__(self, driver, base_url: str):
+        super().__init__(driver)
+        self.url = f"{base_url}/contact"
+
+    def open(self):
+        self.driver.get(self.url)
+
+    def get_title_page(self) -> str:
+        return self.get_text(self._TITLE_LABEL)
+
+    def fill_and_submit_form(self, name, last_name: str, email, phone_number:str, message):
+        # Usamos type_text de tu BasePage para llenar los datos
+        self.type_text(self._NAME_INPUT, name)
+        self.type_text(self._LAST_NAME_INPUT, last_name)
+        self.type_text(self._EMAIL_INPUT, email)
+        self.type_text(self._PHONE_NUMBER_INPUT, phone_number)
+        self.type_text(self._MESSAGE_INPUT, message)
+
+        # Usamos click para enviar
+        self.click(self._SUBMIT_BUTTON)
+
+    def get_success_modal_text(self) -> str:
+        return self.get_text(self._SUCCESS_MODAL_TITLE)
