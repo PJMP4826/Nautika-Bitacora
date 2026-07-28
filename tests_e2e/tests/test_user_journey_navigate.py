354f71ce4e94e5ea1d05e47bddedf420a0dbb159
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-07-28 10:20:28 -0600

    test(e2e): ampliar el recorrido del usuario en la navegación
    - implementar la navegación completa hacia las secciones de zonas y peces
    - añadir interacciones de scroll suave en las nuevas páginas visitadas
    - validar la correcta carga de las URLs al seleccionar elementos aleatorios

--- tests_e2e/tests/test_user_journey_navigate.py
+++ tests_e2e/tests/test_user_journey_navigate.py
@@ -1,12 +1,28 @@
 from pages.home_page import HomePage
+from pages.zones_page import ZonePage
+from pages.fish_page import FishPage
 from pages.contacto_page import ContactoPage
+
 
 def test_user_journey_navigate(driver, base_url):
     # 1. Iniciamos en la página principal
     home_page = HomePage(driver, base_url)
+    zone_page = ZonePage(driver, base_url)
+    fish_page = FishPage(driver, base_url)
+
     home_page.open()
 
     # 2. Scroll vertical
-    home_page.explore_page()
+    home_page.scroll_smooth()
 
+    home_page.go_to_zone()
+
+    zone_page.scroll_smooth(pixels=600, delay=0.02)
+    zone_page.click_random_zone(expected_url_part="/zones/")
+    zone_page.scroll_smooth(pixels=600, delay=0.02)
+
+    home_page.go_to_fish()
     
+    fish_page.scroll_smooth(pixels=600, delay=0.02)
+    fish_page.click_random_fish(expected_url_part="/fish/")
+    fish_page.scroll_smooth(pixels=600, delay=0.02)
