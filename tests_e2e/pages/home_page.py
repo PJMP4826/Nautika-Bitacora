9be880fdeed9e517216dbbb3460c93455062b6d6
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-07-28 09:55:04 -0600

    feat(home_page): añadir métodos de navegación para zonas y peces
    - agregar selectores para los botones de zonas y peces
    - implementar métodos para navegar a las rutas correspondientes
    - incluir esperas de cambio de URL para asegurar la carga de página

--- tests_e2e/pages/home_page.py
+++ tests_e2e/pages/home_page.py
@@ -4,6 +4,8 @@ from pages.base_page import BasePage
 
 class HomePage(BasePage):
     _CONTACT_BUTTON = (By.XPATH, "//button[contains(text(), 'Contacto')]")
+    _ZONES_BUTTON = (By.XPATH, "//button[contains(text(), 'Explorar Zonas')]")
+    _FISH_BUTTON = (By.XPATH, "//button[contains(text(), 'Peces')]")
 
     def __init__(self, driver, base_url: str):
         super().__init__(driver)
@@ -14,6 +16,14 @@ class HomePage(BasePage):
 
     def go_to_contact(self):
         self.click(self._CONTACT_BUTTON)
+
+    def go_to_zone(self):
+        self.click(self._ZONES_BUTTON)
+        self.wait_for_url_change("/zones")
+
+    def go_to_fish(self):
+        self.click(self._FISH_BUTTON)
+        self.wait_for_url_change("/fish")
 
     def explore_page(self):
         # Baja 3000px suavemente
